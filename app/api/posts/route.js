1bfafe46a2bfc3e75fbd979d1a2f1520f4ddde33
AbdelrahmanEhab <abdoehab5008@gmail.com>
2025-02-08 22:20:03 +0200

    get all post new route api

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -2,7 +2,7 @@ import Post from "@/models/Post";
 import { NextResponse } from "next/server";
 import connectDB from "@/utils/connectDb";
 
-// POST Request - Create a new post
+// ! POST Request - Create a new post
 export async function POST(request) {
   // Connect to the database
   await connectDB();
@@ -21,6 +21,29 @@ export async function POST(request) {
     console.error("Error creating post:", error);
     return new NextResponse(
       JSON.stringify({ message: "Failed to create post: " + error.message }),
+      {
+        status: 500, // Server error status code
+        headers: { "Content-Type": "application/json" }, // Add headers
+      }
+    );
+  }
+}
+// ? GET Request - List All Posts
+export async function GET() {
+  // Connect to the database
+  await connectDB();
+
+  try {
+    // Database Operations
+    const posts = await Post.find();
+    return new NextResponse(JSON.stringify({ posts }), {
+      status: 200, // Indicates successful request
+      headers: { "Content-Type": "application/json" }, // Add headers
+    });
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return new NextResponse(
+      JSON.stringify({ message: "Failed to fetch posts: " + error.message }),
       {
         status: 500, // Server error status code
         headers: { "Content-Type": "application/json" }, // Add headers
